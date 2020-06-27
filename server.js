a7b403ea02d210a752d4864ab27ba11af5850ff9
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-06-27 17:16:02 +0200

    update the server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
 
 const db = knex({
     client: 'pg',
@@ -31,18 +32,7 @@ app.get('/', (req, res)=>{
 app.post('/signin', ( req, res) => {signin.handleSignin( req, res, db, bcrypt) })
 app.post('/register', ( req, res) => { register.handleRegister( req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res)=> {
-    const {id} = req.params;
-    db.select('*').from('users').where({id})
-      .then(user => {
-          if(user.length) {
-              res.json(user[0])
-          } else {
-              res.status(400).json('Not found')
-          }
-      })
-      .catch(err => res.status(400).json('error getting user'))  
-})
+app.get('/profile/:id', ( req, res ) => {profile.handleProfile( req, req, db ) })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
