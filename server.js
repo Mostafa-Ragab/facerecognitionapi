bf2ba274b74a0960e70bf1c1b03c33e64a80070b
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-06-27 12:22:11 +0200

    change signin code

--- server.js
+++ server.js
@@ -48,10 +48,20 @@ app.get('/', (req, res)=>{
 })
 app.post('/signin', (req, res) => {
     db.select('email', 'hash').from('login')
-    .where('email','='  , req.body.email)
+    .where('email','=', req.body.email)
     .then(data => {
-        
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid) {
+            db.select('*').from('users')
+            .where('email', '=' , req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+
+        }
     })
+    .catch(err => res.status(400).json('wrong credintials'))
 })
 app.post('/register', (req, res)=>{
     const { email, name, password } = req.body;
