c97b2451055df524d389b966a338f3ed0096f10e
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-06-27 11:59:24 +0200

    connecting to login table

--- server.js
+++ server.js
@@ -57,15 +57,25 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res)=>{
     const { email, name, password } = req.body;
     const hash = bcrypt.hashSync(password);
-    db('users')
-    .returning('*')
-    .insert({
-        email: email,
-        name: name,
-        joined: new Date()
-    })
-    .then(user => {
-        res.json(user[0]);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })    
+        })
     })
     .catch(err => res.status(400).json('unable to change'))
 })
